8b3023011dcc8928b43e991e8ab605f9408562f9
zyearn <zhujiashun2010@gmail.com>
2016-10-25 19:01:33 +0800

    part 2&3 in ex5

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,14 +20,9 @@ grad = zeros(size(theta));
 %
 
 
-
-
-
-
-
-
-
-
+J = 1.0/(2*m) * sum((X*theta - y) .^ 2) + lambda/(2*m) * sum(theta(2:end) .^ 2);
+grad = 1/m * X' * (X*theta - y) + lambda/m * theta;
+grad(1) = grad(1) - lambda/m * theta(1);
 
 
 % =========================================================================
