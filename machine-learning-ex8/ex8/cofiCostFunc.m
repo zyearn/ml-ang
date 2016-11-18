a52bb64a31d032e87b3d99f3b8d5f1a882fa60ea
zyearn <zhujiashun2010@gmail.com>
2016-11-18 10:16:11 +0800

    Regularized Gradient

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -43,8 +43,8 @@ Theta_grad = zeros(size(Theta));
 T = ((X * Theta' - Y).^2) .* R;
 J = (1/2) * sum(sum(T, 1), 2) + (lambda/2) * sum(sum(Theta.^2, 1), 2) + (lambda/2) * sum(sum(X.^2, 1), 2);
 
-X_grad = ((X * Theta' - Y).*R) * Theta;
-Theta_grad = ((X * Theta' - Y).*R)' * X;
+X_grad = ((X * Theta' - Y).*R) * Theta + lambda * X;
+Theta_grad = ((X * Theta' - Y).*R)' * X + lambda * Theta;
 
 % =============================================================
 
